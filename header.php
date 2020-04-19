aa958a64414cd0e11954d559a89678e2f25b5fd6
Kanako Nagiri <kanako22n@gmail.com>
2020-04-19 14:48:22 +0900

    テンプレートファイル分割

--- header.php
+++ header.php
@@ -4,14 +4,12 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1" />
   <meta name="keywords" content="共通キーワード" />
-  <meta name="description" content="共通ディスクリプション" />
-  <title>PACIFIC MALL DEVELOPMENT</title>
+  <meta name="description" content="<?php bloginfo('description'); ?>" />
+  <title><?php bloginfo('name'); ?></title>
   <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/common/favicon.ico" />
   <link href="https://fonts.googleapis.com/earlyaccess/notosansjapanese.css" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css?family=Vollkorn:400i" rel="stylesheet" />
-  <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/styles.css" />
-  <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery-3.3.1.min.js"></script>
-  <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/bundle.js"></script>
+  <?php wp_head(); ?>
 </head>
 <body>
   <div class="container">
